ee9bfe360598a8dcc30080094eb2d4c2227ea875
igorquintino <igorcarvalho2907@gmail.com>
2025-05-13 20:52:05 -0300

    ,

--- server.js
+++ server.js
@@ -71,8 +71,8 @@ app.post('/enviar-email', async (req, res) => {
         <p>Dá uma olhada em como ele é por dentro:</p>
 
         <div style="display: flex; flex-wrap: wrap; gap: 10px; justify-content: center; margin: 20px 0;">
-          <img src="https://seulink.com/print1.png" width="180" style="border-radius: 8px;">
-          <img src="https://seulink.com/print2.png" width="180" style="border-radius: 8px;">
+          <img src="img1.png" width="180" style="border-radius: 8px;">
+          <img src="img2.png" width="180" style="border-radius: 8px;">
           <img src="img 1.png" width="180" style="border-radius: 8px;">
           <img src="img 2.png" width="180" style="border-radius: 8px;">
         </div>
